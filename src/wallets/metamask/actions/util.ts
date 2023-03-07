122cb3cc355b77a124cb10b9de3275c0b94e4cb2
Jiaming Li <ljm625@gmail.com>
2023-03-07 22:37:39 +0800

    fix

--- src/wallets/metamask/actions/util.ts
+++ src/wallets/metamask/actions/util.ts
@@ -3,29 +3,23 @@ import { EXTENSION_ID } from '../setup/downloader';
 
 export const performPopupAction = async (page: Page, action: (popup: Page) => Promise<void>): Promise<void> => {
   let popup = null;
-  try{
-    popup = await page.context().waitForEvent('page', { timeout: 5000 }); // Wait for the popup to show up
-  } catch(e){
-    console.log(e)
-    console.log("Failed to get a popup. let's see if it already pops")
-    let pages = page.context().pages();
-    for(let i=pages.length-1;i>=0;i--){
-      let p = pages[i]
-      if(p.url().includes(EXTENSION_ID+"/notification.html")){
-        popup = p;
-        break;
-      }
+  let pages = page.context().pages();
+  for(let i=pages.length-1;i>=0;i--){
+    let p = pages[i]
+    if(p.url().includes(EXTENSION_ID+"/notification.html")){
+      popup = p;
+      break;
     }
-    if(!popup){
-      throw Error("Metamask Cannot find popup page")
-    }
+  }
+  if(!popup){
+    popup = await page.context().waitForEvent('page'); // Wait for the popup to show up
   }
 
   await action(popup);
   if (!popup.isClosed()) 
   {
     try{
-      await popup.waitForEvent('close', { timeout: 5000 });
+      await popup.waitForEvent('close', { timeout: 10000 });
     } catch(e){
       console.log("Popup maybe not closed correctly")
     }
